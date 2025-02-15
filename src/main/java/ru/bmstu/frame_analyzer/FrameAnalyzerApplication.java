246193e28c2ca1a304ca27e8180a8efb7921aee5
posTour <dimidov496@gmail.com>
2025-02-15 23:35:27 +0300

    init

---
+++ src/main/java/ru/bmstu/frame_analyzer/FrameAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package ru.bmstu.frame_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bmstu/frame_analyzer/FrameAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bmstu.frame_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
