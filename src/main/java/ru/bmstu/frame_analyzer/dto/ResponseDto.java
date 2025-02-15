313f0c7f377ffc9475f8450274c4cb230efda809
posTour <dimidov496@gmail.com>
2025-02-15 23:39:20 +0300

    feat: dto ответа

---
+++ src/main/java/ru/bmstu/frame_analyzer/dto/ResponseDto.java
@@ -0,0 +1,17 @@
+package ru.bmstu.frame_analyzer.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ResponseDto {
+
+    private double value1;
+
+    private double value2;
+
+    private double value3;
+
+    private double value4;
+
+    private String text;
+}
